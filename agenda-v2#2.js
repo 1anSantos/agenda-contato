5d0100177f35d59f415716b3070b6f93063a7cde
Ian Santos <64675360+1anSantos@users.noreply.github.com>
2023-11-01 11:36:11 -0300

    Update agenda-v2#2.js

--- agenda-v2#2.js
+++ agenda-v2#2.js
@@ -48,3 +48,40 @@ class ColegaDeTrabalho extends Contato {
         this._departamento = departamento;
     }
 }
+
+// Menu
+let opcao = 0;
+
+while (opcao != 6) {
+    console.log('\n'+
+    '1- Adicionar contato\n'+
+    '2- Visualizar lista\n'+
+    '3- Editar contato\n'+
+    '4- Excluir Contato\n'+
+    '5- Pesquisar Contato\n'+
+    '6- Sair'
+    );
+    opcao = parseInt(prompt('>> '));
+    switch (opcao) {
+        case 1:
+            listaDeContatos.adicionarContato();
+            break;
+        case 2:
+            listaDeContatos.visualizarLista();
+            break;
+        case 3:
+            listaDeContatos.editarContato();
+            break;
+        case 4:
+            listaDeContatos.excluirContato();
+            break;
+        case 5:
+            listaDeContatos.pesquisarContato();
+            break;
+        case 6:
+            console.log("Saindo do Sistema...");
+            break;
+        default:
+            console.log(`A opção ${opcao} é inválida!!!`);
+    }
+}
