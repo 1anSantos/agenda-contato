3146d9adf007be436689f5275f49a749ccd6e611
Ian Santos <iann.silva2006@gmail.com>
2023-11-02 19:03:18 -0300

    simplify

--- agenda-v2.js
+++ agenda-v2.js
@@ -1,13 +1,7 @@
-// const prompt = require("prompt-sync") ({sigint: false});
-import PromptSync from "prompt-sync";
-const prompt = PromptSync();
-
 import Menu from "./src/Menu.js"
 const menu = new Menu();
 
-// console.log(menu)
-
 while (true) {
-    const answer = menu.general();
+    const answer = menu.principal();
     if (!answer) break;
 }
