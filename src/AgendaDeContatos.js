5d5dc4543688d61ef51dc15615e83fbc4a777359
Ian Santos <iann.silva2006@gmail.com>
2023-11-02 21:07:09 -0300

    no general validate

--- src/AgendaDeContatos.js
+++ src/AgendaDeContatos.js
@@ -8,9 +8,7 @@ class AgendaDeContatos {
     }
 
     adicionarContato(contato) {
-        // if (this.verificarContato(contato)) {
-            this._agenda.push(contato);
-        // }
+        this._agenda.push(contato);
     }
 
     pesquisarContatos(nome) {
@@ -31,38 +29,8 @@ class AgendaDeContatos {
             })
             .join("");
 
-        contato[`set${propriedade}`](valor);
+        return contato[`set${propriedade}`](valor);
     }
-    
-    //! Refazer Código 
-    // validateContato(contato) {
-    //     const dados = contato.getDados();
-
-    //     if (isNaN(dados.id) || dados.id < 0)
-    //         return false;
-    //     if (this._agenda.includes(contato => contato.id === dados.id))
-    //         return false;
-        
-    //     for (const nomePropriedade in dados) {
-    //         if (nomePropriedade === "id") continue;
-
-    //         const valorPropriedade = dados[nomePropriedade];
-
-    //         propriedade = propriedade
-    //             .replace(/[_\s]+/g, " ")
-    //             .split(" ")
-    //             .map(word => {
-    //                 word = word.charAt(0).toUpperCase() + word.slice(1);
-    //                 return word;
-    //             })
-    //             .join("");
-
-    //         if (!contato[`validate${propriedade}`](valorPropriedade))
-    //             return false;
-    //     }
-        
-    //     return true;
-    // }
 }
 
 export default AgendaDeContatos;
